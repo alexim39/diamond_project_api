1836c2087ac8c92709e0434e08b3625f1aa80f44
Alex Imenwo <aleximenwo@gmail.com>
2024-09-25 15:53:52 +0100

    updated survey email temp

--- src/services/templates/survey/ownerTemplate.js
+++ src/services/templates/survey/ownerTemplate.js
@@ -14,14 +14,14 @@ export const ownerEmailTemplate = (surveyData) => `
       <p><strong>Email address:</strong> ${surveyData.email}</p>
       <p><strong>Phone number:</strong> ${surveyData.phoneNumber}</p>
       <br>
-      <a href="https://partners.diamondprojectonline.com/" style="padding: 10px 20px; background-color: #007BFF; color: white; text-decoration: none; border-radius: 5px; text-align: center; margin: 1em 0;">Go to Partner Dashboard</a>
+      <div style="text-align: center;">
+        <a href="https://partners.diamondprojectonline.com/" style="padding: 10px 20px; background-color: #007BFF; color: white; text-decoration: none; border-radius: 5px; text-align: center; margin: 1em 0;">Go to Partner Dashboard</a>
+      </div>
     </main>
-    <footer style="text-align: center; padding: 20px; background-color: #f4f4f4;">
+    <br>
+    <footer style="text-align: center; padding: 20px; background-color: #f4f4f4; ; margin-top: 20px;"">
       <p>Follow us on:
         <a href="https://www.facebook.com/profile.php?id=61561933352527" style="margin: 0 5px;">Facebook</a> |
-        /* <a href="https://twitter.com/diamondproject" style="margin: 0 5px;">LinkedIn</a> | */
-        /* <a href="https://twitter.com/diamondproject" style="margin: 0 5px;">Twitter</a> | */
-       /*  <a href="https://instagram.com/diamondproject" style="margin: 0 5px;">Instagram</a> */
       </p>
       <p><a href="https://diamondprojectonline.com/legal/privacy">Privacy</a> | 
         <a href="https://diamondprojectonline.com/legal/terms">Terms</a> | 
--- src/services/templates/survey/userTemplate.js
+++ src/services/templates/survey/userTemplate.js
@@ -10,14 +10,14 @@ export const userWelcomeEmailTemplate = (surveyData) => `
       <p>Thank you for your interest in Diamond Project! We are excited to have you on board.</p>
       <p>If you have any questions, please visit our <a href="https://diamondprojectonline.com/faq" style="color: #007BFF;">FAQ page</a> for answers to common questions.</p>
       <br>
-      <a href="https://diamondprojectonline.com/dashboard" style="padding: 10px 20px; background-color: #28A745; color: white; text-decoration: none; border-radius: 5px; text-align: center; margin: 1em 0;">Visit Your Dashboard</a>
+      <div style="text-align: center;">
+        <a href="https://diamondprojectonline.com/dashboard" style="padding: 10px 20px; background-color: #28A745; color: white; text-decoration: none; border-radius: 5px; text-align: center; margin: 1em 0;">Visit Your Dashboard</a>
+      </div>
     </main>
-    <footer style="text-align: center; padding: 20px; background-color: #f4f4f4;">
+    <br>
+    <footer style="text-align: center; padding: 20px; background-color: #f4f4f4; margin-top: 20px;">
      <p>Follow us on:
         <a href="https://www.facebook.com/profile.php?id=61561933352527" style="margin: 0 5px;">Facebook</a> |
-        /* <a href="https://twitter.com/diamondproject" style="margin: 0 5px;">LinkedIn</a> | */
-        /* <a href="https://twitter.com/diamondproject" style="margin: 0 5px;">Twitter</a> | */
-       /*  <a href="https://instagram.com/diamondproject" style="margin: 0 5px;">Instagram</a> */
       </p>
       <p><a href="https://diamondprojectonline.com/legal/privacy">Privacy</a> | 
         <a href="https://diamondprojectonline.com/legal/terms">Terms</a> | 
