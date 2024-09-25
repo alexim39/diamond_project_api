5f26596ff8cedaf258af5486aa4571e1f321d6c5
Alex Imenwo <aleximenwo@gmail.com>
2024-09-25 15:42:47 +0100

    updated the email template

--- src/services/templates/survey/ownerTemplate.js
+++ src/services/templates/survey/ownerTemplate.js
@@ -1,25 +1,27 @@
 export const ownerEmailTemplate = (surveyData) => `
   <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto;">
     <header style="text-align: center; padding: 10px; background-color: #f4f4f4;">
-      <img src="https://diamondprojectonline.com/logo.png" alt="Diamond Project" style="width: 150px;">
+      /* <img src="https://diamondprojectonline.com/logo.png" alt="Diamond Project" style="width: 150px;"> */
+      <span style="font-family: sans-serif; font-size: 20px; font-weight: bold; color: black;">Diamond Project Online</span>
     </header>
     <main style="padding: 20px;">
       <h1>Survey Submission</h1>
       <p>A prospect named <strong>${surveyData.name} ${surveyData.surname}</strong> with phone number <strong>${surveyData.phoneNumber}</strong> just submitted the survey form from <a href="https://diamondprojectonline.com">Diamond Project Online</a>.</p>
       <p>You may need to follow up with the user via WhatsApp or phone call.</p>
-      <h2>Complete Prospect Response</h2>
+      <h2>Prospect Contact Details</h2>
       <p><strong>Name:</strong> ${surveyData.name}</p>
       <p><strong>Surname:</strong> ${surveyData.surname}</p>
       <p><strong>Email address:</strong> ${surveyData.email}</p>
       <p><strong>Phone number:</strong> ${surveyData.phoneNumber}</p>
       <br>
-      <a href="https://diamondprojectonline.com" style="padding: 10px 20px; background-color: #007BFF; color: white; text-decoration: none; border-radius: 5px;">Go to Dashboard</a>
+      <a href="https://partners.diamondprojectonline.com/" style="padding: 10px 20px; background-color: #007BFF; color: white; text-decoration: none; border-radius: 5px; text-align: center; margin: 1em 0;">Go to Partner Dashboard</a>
     </main>
     <footer style="text-align: center; padding: 20px; background-color: #f4f4f4;">
       <p>Follow us on:
-        <a href="https://facebook.com/diamondproject" style="margin: 0 5px;">Facebook</a> |
-        <a href="https://twitter.com/diamondproject" style="margin: 0 5px;">Twitter</a> |
-        <a href="https://instagram.com/diamondproject" style="margin: 0 5px;">Instagram</a>
+        <a href="https://www.facebook.com/profile.php?id=61561933352527" style="margin: 0 5px;">Facebook</a> |
+        /* <a href="https://twitter.com/diamondproject" style="margin: 0 5px;">LinkedIn</a> | */
+        /* <a href="https://twitter.com/diamondproject" style="margin: 0 5px;">Twitter</a> | */
+       /*  <a href="https://instagram.com/diamondproject" style="margin: 0 5px;">Instagram</a> */
       </p>
       <p><a href="https://diamondprojectonline.com/legal/privacy">Privacy</a> | 
         <a href="https://diamondprojectonline.com/legal/terms">Terms</a> | 
--- src/services/templates/survey/userTemplate.js
+++ src/services/templates/survey/userTemplate.js
@@ -1,7 +1,8 @@
 export const userWelcomeEmailTemplate = (surveyData) => `
   <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto;">
     <header style="text-align: center; padding: 10px; background-color: #f4f4f4;">
-      <img src="https://diamondprojectonline.com/logo.png" alt="Diamond Project" style="width: 150px;">
+      /* <img src="https://diamondprojectonline.com/logo.png" alt="Diamond Project" style="width: 150px;"> */
+      <span style="font-family: sans-serif; font-size: 20px; font-weight: bold; color: black;">Diamond Project Online</span>
     </header>
     <main style="padding: 20px;">
       <h1>Welcome to Diamond Project</h1>
@@ -9,13 +10,14 @@ export const userWelcomeEmailTemplate = (surveyData) => `
       <p>Thank you for your interest in Diamond Project! We are excited to have you on board.</p>
       <p>If you have any questions, please visit our <a href="https://diamondprojectonline.com/faq" style="color: #007BFF;">FAQ page</a> for answers to common questions.</p>
       <br>
-      <a href="https://diamondprojectonline.com/dashboard" style="padding: 10px 20px; background-color: #28A745; color: white; text-decoration: none; border-radius: 5px;">Visit Your Dashboard</a>
+      <a href="https://diamondprojectonline.com/dashboard" style="padding: 10px 20px; background-color: #28A745; color: white; text-decoration: none; border-radius: 5px; text-align: center; margin: 1em 0;">Visit Your Dashboard</a>
     </main>
     <footer style="text-align: center; padding: 20px; background-color: #f4f4f4;">
-      <p>Follow us on:
-        <a href="https://facebook.com/diamondproject" style="margin: 0 5px;">Facebook</a> |
-        <a href="https://twitter.com/diamondproject" style="margin: 0 5px;">Twitter</a> |
-        <a href="https://instagram.com/diamondproject" style="margin: 0 5px;">Instagram</a>
+     <p>Follow us on:
+        <a href="https://www.facebook.com/profile.php?id=61561933352527" style="margin: 0 5px;">Facebook</a> |
+        /* <a href="https://twitter.com/diamondproject" style="margin: 0 5px;">LinkedIn</a> | */
+        /* <a href="https://twitter.com/diamondproject" style="margin: 0 5px;">Twitter</a> | */
+       /*  <a href="https://instagram.com/diamondproject" style="margin: 0 5px;">Instagram</a> */
       </p>
       <p><a href="https://diamondprojectonline.com/legal/privacy">Privacy</a> | 
         <a href="https://diamondprojectonline.com/legal/terms">Terms</a> | 
